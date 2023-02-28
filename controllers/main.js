c5701e1b45669470593574113459d43995a392ac
Luong Vo <diamonluong>
2023-02-28 22:31:35 +0700

    Tâm

--- controllers/main.js
+++ controllers/main.js
@@ -49,6 +49,12 @@ function updateProduct(productId) {
     type: getElement("#type").value,
   };
 
+  let isValid = validate();
+  // isValid là false => form không hợp lệ => không cho phép tạo student bằng cách kết thúc hàm
+  if (!isValid) {
+    return;
+  }
+
   apiUpdateProduct(productId, product)
     .then((response) => {
       getProducts();
@@ -99,6 +105,12 @@ function createProduct() {
     description: getElement("#desc").value,
     type: getElement("#type").value,
   };
+
+  let isValid = validate();
+  // isValid là false => form không hợp lệ => không cho phép tạo student bằng cách kết thúc hàm
+  if (!isValid) {
+    return;
+  }
 
   apiCreateProducts(product)
     .then((response) => {
@@ -166,7 +178,141 @@ function renderProducts(products) {
 //   getProducts(searchValue);
 // });
 
-// // Helper
-// function getElement(selector) {
-//   return document.querySelector(selector);
-// }
+// Helper
+function getElement(selector) {
+  return document.querySelector(selector);
+}
+
+function validate() {
+  // Mặc định là form hợp lệ
+  let isValid = true;
+
+  //   Kiểm tra tên điện thoại
+  let name = getElement("#name").value;
+  if (!name.trim()) {
+    isValid = false;
+    getElement("#tbname").innerHTML =
+      "Tên điện thoại không được để khoản trống";
+    getElement("#tbname").style.display = "inline";
+  }
+  // Kiểm tra giá tiền
+  let price = getElement("#price").value;
+  if (!price.trim()) {
+    isValid = false;
+    getElement("#tbprice").innerHTML = "Giá tiền không được để khoản trống";
+    getElement("#tbprice").style.display = "inline";
+  } else if (!/^([1-9]|[1-9][0-9]{1,3}|1[0-9]{1,4}|20000)$/.test(price)) {
+    isValid = false;
+    getElement("#tbprice").innerHTML = "Giá tiền phải là số";
+    getElement("#tbprice").style.display = "inline";
+  } else {
+    getElement("#tbprice").innerHTML = "";
+    getElement("#tbprice").style.display = "inline";
+  }
+
+  // Kiểm tra màn hình
+  let screen = getElement("#screen").value;
+  if (!screen.trim()) {
+    isValid = false;
+    getElement("#tbscreen").innerHTML = "Màn hình không được để khoản trống";
+    getElement("#tbscreen").style.display = "inline";
+  } else if (!/^[a-zA-Z]+$/.test(screen)) {
+    isValid = false;
+    getElement("#tbscreen").innerHTML = "Màn hình phải là chữ";
+    getElement("#tbscreen").style.display = "inline";
+  } else {
+    getElement("#tbscreen").innerHTML = "";
+    getElement("#tbscreen").style.display = "inline";
+  }
+
+  //   Kiểm tra camera trước
+  let backCam = getElement("#backCam").value;
+  if (!backCam.trim()) {
+    isValid = false;
+    getElement("#tbbackCam").innerHTML =
+      "Camera trước không được để khoản trống";
+    getElement("#tbbackCam").style.display = "inline";
+  }
+
+  //   Kiểm tra camera sau
+  let frontCam = getElement("#frontCam").value;
+  if (!frontCam.trim()) {
+    isValid = false;
+    getElement("#tbfrontCam").innerHTML =
+      "Camera sau không được để khoản trống";
+    getElement("#tbfrontCam").style.display = "inline";
+  }
+
+  // Kiểm tra đường dẫn hình ảnh
+  let img = getElement("#img").value;
+  if (!img.trim()) {
+    isValid = false;
+    getElement("#tbimg").innerHTML = "Hình ảnh không được để khoản trống";
+    getElement("#tbimg").style.display = "inline";
+  } else if (!/http/.test(img)) {
+    isValid = false;
+    getElement("#tbimg").innerHTML = "Hình ảnh phải là 1 đường dẫn: http";
+    getElement("#tbimg").style.display = "inline";
+  } else {
+    getElement("#tbimg").innerHTML = "";
+    getElement("#tbimg").style.display = "inline";
+  }
+
+  //   Kiểm tra camera sau
+  let desc = getElement("#desc").value;
+  if (!desc.trim()) {
+    isValid = false;
+    getElement("#tbdesc").innerHTML =
+      "Tình trạng máy không được để khoản trống";
+    getElement("#tbdesc").style.display = "inline";
+  }
+
+  return isValid;
+}
+
+function sortPrice(searchValue) {
+  apiGetProducts(searchValue)
+    .then((response) => {
+      // Call API thành công
+      const product = response.data.map((product) => {
+        return new Products(
+          product.id,
+          product.name,
+          product.price,
+          product.screen,
+          product.backCam,
+          product.frontCam,
+          product.img,
+          product.description,
+          product.type
+        );
+      });
+      sortByPrice(product);
+      renderProducts(products);
+    })
+    .catch((error) => {
+      // Call API thất bại
+      alert("API get products error");
+    });
+}
+function searchNV() {
+  // B1: DOM
+  let search = getElement("#searchName").value;
+
+  // B2: Lọc những student có name khớp với giá trị search
+  let newProductList = product.filter((account) => {
+    let xeploai = account.sort().toLowerCase();
+    search = search.toLowerCase();
+
+    return xeploai.indexOf(search) !== -1;
+  });
+
+  // B3: Gọi hàm renderTable để hiển thị ra giao diện
+  renderProducts(newProductList);
+}
+function sortByPrice(products) {
+  products.sort(function (a, b) {
+    return a.price - b.price;
+  });
+  return products;
+}
